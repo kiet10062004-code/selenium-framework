3f35a02d5a326812d69b4fc4793130a96100188d
nguyen kiet <kiet10062004@gmail.com>
2026-03-31 14:56:05 +0700

    fix carttest and cartpage

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,6 +1,9 @@
 import framework.base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import framework.pages.CartPage;
+import framework.pages.InventoryPage;
 import framework.pages.LoginPage;
 
 public class CartTest extends BaseTest {
@@ -17,23 +20,32 @@ public class CartTest extends BaseTest {
 
     @Test
     public void removeItem() {
-        int count = new LoginPage(getDriver())
-                .login("standard_user", "secret_sauce")
-                .addFirstItemToCart()
-                .goToCart()
-                .removeFirstItem()
-                .getItemCount();
+        LoginPage login = new LoginPage(getDriver());
 
-        Assert.assertEquals(count, 1);
+        InventoryPage inventory = login.login("standard_user", "secret_sauce");
+
+        inventory.addFirstItemToCart();
+
+        CartPage cart = inventory.goToCart();
+
+        cart.removeFirstItem();
+
+        Assert.assertEquals(cart.getItemCount(), 0);
     }
 
     @Test
     public void cartEmpty() {
-        int count = new LoginPage(getDriver())
-                .login("standard_user", "secret_sauce")
-                .goToCart()
-                .getItemCount();
+        LoginPage login = new LoginPage(getDriver());
 
-        Assert.assertEquals(count, 1);
+        InventoryPage inventory = login.login("standard_user", "secret_sauce");
+
+        Assert.assertTrue(inventory.isLoaded());
+
+        inventory.addFirstItemToCart();
+
+        CartPage cart = inventory.goToCart();
+
+        Assert.assertEquals(cart.getItemCount(), 1);
     }
+
 }
